71d99fd436327e02e8957eb7619fdc76f368a20e
Bad-Raider <stanislav.gusiev@gmail.com>
2023-03-05 11:10:06 +0200

    finish home work
    tast-1
    task-2
    
    OK
    
    PS:
    "next, i want add lazy pictures for my home work"

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -17,3 +17,30 @@ const card = galleryItems.map(obj => {
 // Edding card in the gallary
 galleryContainer.insertAdjacentHTML("beforeend", card);
 
+// handler show modal
+const handleOpenModalShow = (event) => {
+    
+    // click on img only
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+    // default link open
+    event.preventDefault();
+   //options for light box
+    const options = {
+    captionsData: "alt",
+    showCounter: false
+    };
+    // simple light box
+    const lightbox = new SimpleLightbox('.gallery a', options);
+    lightbox.on('show.simplelightbox'); 
+}
+
+galleryContainer.addEventListener("click", handleOpenModalShow);
+
+
+
+
+
+
+
