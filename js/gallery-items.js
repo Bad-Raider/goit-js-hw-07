32197f23e82df36606a87fb78286e6ee388ac567
Bad-Raider <stanislav.gusiev@gmail.com>
2023-03-05 12:26:31 +0200

    preparation lazy pictures
    created array with new pictures

--- js/gallery-items.js
+++ js/gallery-items.js
@@ -62,4 +62,70 @@ export const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/08/11/23/48/mountains-1587287_1280.jpg',
+    description: 'Mountains',
+  },
+{
+    preview:
+      'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg',
+    description: 'Lake',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_1280.jpg',
+    description: 'Woods',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/09/21/14/39/surface-455124__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/09/21/14/39/surface-455124__480.jpg',
+    description: 'Rain',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/29/04/19/ocean-1867285__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/29/04/19/ocean-1867285__480.jpg',
+    description: 'See',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2014/12/21/15/44/raindrops-574971_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2014/12/21/15/44/raindrops-574971_1280.jpg',
+    description: 'Ladybug',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/14/05/29/children-1822704_960_720.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/14/05/29/children-1822704_1280.jpg',
+    description: 'Happy children',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/12/02/07/07/autumn-4667080_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/12/02/07/07/autumn-4667080_1280.jpg',
+    description: 'Trees',
+  },
+    {
+    preview:
+      'https://cdn.pixabay.com/photo/2017/12/18/09/54/lago-di-limides-3025780_640.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2017/12/18/09/54/lago-di-limides-3025780_1280.jpg',
+    description: 'Trees and lake',
+  },
+
 ];
+
+  
